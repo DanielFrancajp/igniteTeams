f17ef90c66bc674664d17bfeabfd7edcba66be17
unknown <danieldenotas@gmail.com>
2023-03-06 16:31:39 -0300

    create screens GroupCard,Header, Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,20 @@
-import {
-    Container,
-} from './styles';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
+import { Container } from './styles';
 
 export function Groups() {
     return (
         <Container>
+            <Header />
+            <Highlight
+                title='Turmas'
+                subtitle='jogue com sua turma!'
+            />
+            <GroupCard
+                title='Galera do ignite '
+            />
         </Container>
     );
 }
