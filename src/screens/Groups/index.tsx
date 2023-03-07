67b1e6e9ca34666376cba8958269644ae03fab6b
DanielFrancajp <danieldenotas@gmail.com>
2023-03-06 22:47:46 -0300

    create component ListEmpety and Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpety } from '@screens/ListEmpety';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
+
+    const [groups, setGroups] = useState<string[]>([]);
+
     return (
         <Container>
             <Header />
@@ -12,9 +21,25 @@ export function Groups() {
                 title='Turmas'
                 subtitle='jogue com sua turma!'
             />
-            <GroupCard
-                title='Galera do ignite '
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() =>
+                    <ListEmpety message='Que tal cadastrar a primeira turma?' />
+                }
             />
+
+            <Button
+                title='Criar nova turma'
+            />
+
         </Container>
     );
 }
