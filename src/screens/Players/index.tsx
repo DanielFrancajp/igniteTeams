117a90731c899ffdad1e7476e5801907236ba46e
DanielFrancajp <danieldenotas@gmail.com>
2023-03-16 11:13:39 -0300

    Create component filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Container, Form, NumberOfPlayers } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { HeaderList } from './styles';
 
 export function Players() {
+
+
+    const [team, setTeam] = useState('Time A')
+    const [players, setPlayers] = useState([]);
+
     return (
         <Container>
 
@@ -14,7 +25,34 @@ export function Players() {
                 subtitle="adicione a galera e separe os times"
             />
 
-            <ButtonIcon/>
+            <Form>
+                <Input
+                    placeholder="Nome da pessoa"
+                    autoCorrect={false}
+                />
+                <ButtonIcon
+                    icon="add"
+                />
+            </Form>
+
+
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
 
         </Container>
     )
